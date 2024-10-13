bd6c7fa454a086bd45e58b446ef08ca2dd2ed479
ist1104126 <ist1104126@tecnico.ulisboa.pt>
2024-10-13 22:06:11 +0100

    feat(leaderboard): created leaderboard microservice

---
+++ app/leaderboard/index.ts
@@ -0,0 +1,43 @@
+import redis from 'redis';
+
+const REDIS_URL = Bun.env.REDIS_URL || "redis://localhost:6379";
+
+const client =
+    await redis.createClient({ url: REDIS_URL })
+        .on('error', error => console.error('Redis client error:', error))
+        .connect();
+
+const server = Bun.serve({
+    port: 8000,
+    async fetch(req) {
+        const url = new URL(req.url);
+
+        if (url.pathname === "/") {
+            if (req.method === "GET") {
+                const leaderboard = await client.ZRANGE_WITHSCORES("leaderboard", 0, -1, { REV: true }) ?? [];
+                return Response.json(leaderboard, { status: 200 });
+            }
+            
+            if (req.method === "POST") {
+                const params = url.searchParams;
+                
+                const score = params.get("score");
+                if (!score) {
+                    return new Response("Score paramenter not found");
+                }
+
+                const uuid = params.get("uuid");
+                if (!uuid) {
+                    return new Response("UUID paramenter not found");
+                }
+
+                await client.ZADD("leaderboard", { score: parseInt(score), value: uuid });
+                return new Response("Success", { status: 201 });
+            }
+            
+            return new Response("Method not allowed", { status: 405 })
+        }
+            
+        return new Response("Page not found", { status: 404 });
+    }
+});
