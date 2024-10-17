94b59da361cb5148e9e881d6ce05f49ac360d708
Lucas Pinto <lucas.f.pinto@tecnico.ulisboa.pt>
2024-10-17 14:12:42 +0100

    fix flake

--- flake.nix
+++ flake.nix
@@ -9,12 +9,16 @@
   outputs = inputs: inputs.utils.lib.eachDefaultSystem (system:
     let
       pkgs = import inputs.nixpkgs { inherit system; };
+      gdk = pkgs.google-cloud-sdk.withExtraComponents( with pkgs.google-cloud-sdk.components; [
+        gke-gcloud-auth-plugin
+      ]);
     in
     {
       devShell = pkgs.mkShell {
         buildInputs = with pkgs; [
           vagrant
-          google-cloud-sdk
+          gdk
+          kubectl
         ];
      };
   });
