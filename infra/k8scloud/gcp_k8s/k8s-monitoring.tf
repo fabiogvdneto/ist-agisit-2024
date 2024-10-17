53ce8ea1e1493cf9f2dfb2afb5ce558df5a73831
Lucas Pinto <lucas.f.pinto@tecnico.ulisboa.pt>
2024-10-17 14:13:02 +0100

    working graphana

---
+++ infra/k8scloud/gcp_k8s/k8s-monitoring.tf
@@ -0,0 +1,30 @@
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
+resource "helm_release" "grafana" {
+  name = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart = "grafana"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+
+  set {
+    name = "adminUser"
+    value = "admin"
+  }
+
+  set {
+    name = "adminPassword"
+    value = "admin"
+  }
+}
+
+resource "helm_release" "kube-prometheus" {
+  name = "prometheus"
+  namespace  = kubernetes_namespace.monitoring.metadata[0].name
+  repository = "https://prometheus-community.github.io/helm-charts"
+  version = "25.24.1"
+  chart = "prometheus"
+}
