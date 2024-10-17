b357d81d20d8ec2b24a3eadeb6973ff53de819a5
Lucas Pinto <lucas.f.pinto@tecnico.ulisboa.pt>
2024-10-17 16:29:48 +0100

    finishing touches?

--- infra/k8scloud/gcp_k8s/k8s-monitoring.tf
+++ infra/k8scloud/gcp_k8s/k8s-monitoring.tf
@@ -7,4 +7,5 @@ resource "helm_release" "kube-prometheus" {
   name       = "prom-operator"
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "kube-prometheus-stack"
+  namespace = kubernetes_namespace.monitoring.metadata.0.name
 }
