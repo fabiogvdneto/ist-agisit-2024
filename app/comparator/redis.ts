48f00c61137e2c395ec34e9c204cf6ad95ebc02d
Lucas Pinto <lucas.f.pinto@tecnico.ulisboa.pt>
2024-10-23 15:01:28 +0100

    redis leader and follower on app

--- app/comparator/redis.ts
+++ app/comparator/redis.ts
@@ -1,22 +1,28 @@
 import { createClient } from "npm:redis@^4.5";
 
 const REDIS_URL = Deno.env.get("REDIS_URL") || "redis://localhost:6379";
+const REDIS_FOLLOWER = Deno.env.get("REDIS_FOLLOWER") || "redis://localhost:6379";
 
 // make a connection to the local instance of redis
-const client = createClient({
+const leader = createClient({
     url: REDIS_URL,
 });
 
-await client.connect();
+const follower = createClient({
+    url: REDIS_FOLLOWER,
+});
+
+await leader.connect();
+await follower.connect();
 
 export const write = async (key: string, value: string|number) => {
-    return client.set(key, value);
+    return leader.set(key, value);
 }
 
 export const read = async (key: string) => {
-    return client.get(key);
+    return follower.get(key);
 }
 
 export const del = async (key: string) => {
-    return client.del(key);
+    return leader.del(key);
 }
